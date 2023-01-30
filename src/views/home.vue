7a99a99f501ffb6a23718ed91398003d4a41ddab
younglina <younglina0409@outlook.com>
2023-01-30 20:03:52 +0800

    feat: vue-router unocss

---
+++ src/views/home.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const toHome = () => {
+  router.push('/page')
+}
+
+</script>
+
+<template>
+  <div>
+    home
+    <div p-5 text-red i-mdi-food-takeout-box>123</div>
+    <button @click="toHome">asdf</button>
+  </div>
+</template>
---
+++ src/views/page.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    detail
+  </div>
+</template>
