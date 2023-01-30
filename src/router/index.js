2b322d916e5d4e1fcd26e43c9ac0aeb358c4d4cd
younglina <younglina0409@outlook.com>
2023-01-30 21:12:49 +0800

    feat: 自适应

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-const Home = import('@/views/home.vue')
-const Page = import('@/views/page.vue')
+const Home = () => import('@/views/home.vue')
+const Page = () => import('@/views/page.vue')
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/page', name: 'Page', component: Page },
