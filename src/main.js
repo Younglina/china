5fdc26b3df0d82d4f6df50a8f17d3639b6f55424
younglina <younglina0409@outlook.com>
2023-01-31 17:04:29 +0800

    feat: vant

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import './utils/flexible.js'
-
+import { createPinia } from 'pinia'
 // 导入Unocss
 import 'uno.css'
-createApp(App).use(router).mount('#app')
+import vconsole from 'vconsole'
+new vconsole()
+const app = createApp(App)
+
+app.use(router)
+app.use(createPinia())
+app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -4,29 +4,36 @@ import vue from '@vitejs/plugin-vue'
 // 引入Unocss
 import Unocss from 'unocss/vite';
 import { presetUno, presetAttributify, presetIcons } from 'unocss'
-
+//自适应
 import postCssPxToRem from "postcss-pxtorem"
 import autoprefixer from 'autoprefixer'
+
+// vant按需引入
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      resolvers: [VantResolver()],
+    }),
     Unocss({ // 使用Unocss
       presets: [
         presetUno(),
         presetAttributify(),
         presetIcons()],
-    })
+    }),
   ],
   css: {
     postcss: {
       plugins: [
-        postCssPxToRem({
-          rootValue: 16, // 1rem的大小
-          propList: ['*'], // 需要转换的属性，这里选择全部都进行转换
-          unitPrecision: 5,
-        }),
+        // postCssPxToRem({
+        //   rootValue: 16, // 1rem的大小
+        //   propList: ['*'], // 需要转换的属性，这里选择全部都进行转换
+        //   unitPrecision: 5,
+        // }),
         autoprefixer({ // 自动添加前缀
           overrideBrowserslist: [
             "Android 4.1",
