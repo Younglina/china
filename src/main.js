5011828ca741b6c71a073bd62eb66f10d3036173
Younglina <Younglina@Outlook.com>
2023-02-12 22:53:00 +0800

    feat: merge

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router/index'
+import './utils/flexible.js'
+import { createPinia } from 'pinia'
+import './style.css';
+import { initData } from '@/utils/useData.js'
+const app = createApp(App)
+// initData()
+
+app.use(router)
+app.use(createPinia())
+app.mount('#app')
---
+++ src/views/page.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { collection, doc, getDoc, getDocs, setDoc } from "firebase/firestore";
+import { database } from '@/firebase'
+import { useFirestore } from '@vueuse/firebase/useFirestore'
+const fireRef = collection(database, 'comment')
+const querySnapshot = await getDocs(fireRef, 'txc');
+querySnapshot.forEach((doc) => {
+  console.log(doc.id, " => ", doc.data());
+});
+</script>
+
+<template>
+  <div>
+    <button @click="writeUserData">writeUserData</button>
+  </div>
+</template>
