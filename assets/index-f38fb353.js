6cff3488895491e397bc4f2e7c3fbc0eaba7a84b
汪志强 <younglina0409@outlook.com>
2023-03-01 13:42:18 +0800

    deploy

--- assets/index-f38fb353.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./function-call-ae4c5185.js";import{v as a}from"./index-87e8ff50.js";a(t);
---
+++ assets/index-fbda6501.js
@@ -0,0 +1,1 @@
+import{b as t}from"./function-call-742c5b99.js";import{M as a}from"./index-45ba34f0.js";a(t);
