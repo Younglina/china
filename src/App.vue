d41930742a0c3e8ba250ee7ba1785d595213fe45
younglina <younglina0409@outlook.com>
2023-03-30 20:59:50 +0800

    feat: permission

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, watch, computed } from 'vue'
 import { initData, tabs } from '@/utils/useData.js'
+import { useStore } from '@/store'
 import { useRoute, useRouter } from 'vue-router'
 initData()
 const route = useRoute()
@@ -8,11 +9,18 @@ const router = useRouter()
 const active = ref('');
 let title = ref("");
 let hasBack = ref(false)
+const store = useStore()
+const cmptabs = computed(()=>{
+  const temTabs = [...tabs]
+  if(store.userInfo && store.userInfo.permission.includes(import.meta.env.VITE_VRF))
+    temTabs.push({ name: 'Verify', path: '/verify', label: '审核', icon: 'user-o' })
+  return temTabs
+})
 watch(route, (val) => {
   hasBack = !!history.state.back
   title.value = val.meta.title
   active.value = val.name
-  if (!tabs.some(t => t.name === val.name)) {
+  if (!cmptabs.value.some(t => t.name === val.name)) {
     active.value = ''
   }
 }, {immediate: true})
@@ -40,7 +48,7 @@ const onClickLeft = () => {
     </router-view>
   </main>
   <van-tabbar v-model="active">
-    <van-tabbar-item v-for="item in tabs" :key="item.name" :name="item.name" :to="item.path" :icon="item.icon">
+    <van-tabbar-item v-for="item in cmptabs" :key="item.name" :name="item.name" :to="item.path" :icon="item.icon">
       {{ item.label }}
     </van-tabbar-item>
   </van-tabbar>
